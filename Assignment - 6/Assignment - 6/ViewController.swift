5b339699742f5feedea833ef6b3722b025b07458
Aye Myat Min <ayemyat.minn@mm-digital-solutions.com>
2019-09-14 14:23:37 +0630

    amm:project set up

---
+++ Assignment - 6/Assignment - 6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment - 6
+//
+//  Created by Aye Myat Minn on 9/14/19.
+//  Copyright © 2019 Aye Myat Minn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
